8414e0958a303e06963ef2276d22704d518d5f81
faith <jamienibtong@gmail.com>
2021-05-03 20:11:51 +0800

    added display window

--- qrsh/__init__.py
+++ qrsh/__init__.py
@@ -10,8 +10,11 @@ def _decode(frame):
     if frame is not None:
         barcodes = decode(frame)
         for barcode in barcodes:
-            barcode_data = barcode.data.decode('utf-8')
+            barcode_data = barcode.data.decode("utf-8")
             if barcode_data:
-                if barcode_data.startswith('http://') \
-                        or barcode_data.startswith('https://'):
-                     webbrowser.open(barcode_data)
+                if barcode_data.startswith("http://") or barcode_data.startswith(
+                    "https://"
+                ):
+                    webbrowser.open(barcode_data)
+                else:
+                    print(barcode_data)
